935394e4104a8452d4d67d971fc8b9ad42212066
Michael Wooten <mwooten.dev@gmail.com>
2020-04-15 23:13:38 -0400

    Schematic updated to add husky dependency.

--- src/husky/index.ts
+++ src/husky/index.ts
@@ -1,10 +1,12 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { addPackageJsonDependency, NodeDependencyType } from 'schematics-utilities';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function husky(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    addPackageJsonDependency(tree, { type: NodeDependencyType.Dev, name: 'husky', version: '^4.2.5'})
     return tree;
   };
 }
